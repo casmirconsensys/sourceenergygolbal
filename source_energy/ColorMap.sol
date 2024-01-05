5c281eeea586da098689e63fe1e2b84dd7b15814
@askcasmir <casmirconsults@gmail.com>
2024-01-05 17:58:01 -0500

    WIP:ColorMap.sol Smart Contract

---
+++ source_energy/ColorMap.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.6.0;
+
+contract ColorMap {
+
+    struct Sound {
+        // INDIV 100% #FFFFFF   100% ENERGY FF FF FF,
+        UltraSound:7
+        Very_High: 6
+        High:5
+        Upper-Range:4
+        Mid_Range:3
+        Low_Mid: 2
+        Bass:1
+        Sub_Base:0
+    }
+    struct Light {
+        #FF: 100%
+        #EO: 888%
+        #BF: 75%
+        #A1: 63%
+        #80: 50%
+        #61: 38%
+        #40: 25%
+        #21: 13%
+    }
+    
+    struct Individuals {
+
+    }
+    struct Organizaions {
+
+    }
+    
+}
